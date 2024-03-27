1e601f0967e242c06766249f90b721614304f656
Vishal Dolare <mobile.vishal17@gmail.com>
2024-03-27 19:13:43 +0530

    Added by feature branch

--- src/main/java/com/java/fileoperation/GitOperations.java
+++ src/main/java/com/java/fileoperation/GitOperations.java
@@ -2,8 +2,12 @@ package com.java.fileoperation;
 
 public class GitOperations {
 
+	public void fadded() {
+		System.out.println("Added by feature branch");
+	}
 	public static void main(String[] args) {
-		System.out.println("Base code...!");
+		System.out.println("Removed by feature branch");
+		System.out.println("Added by feature branch");
 		
 	}
 }
