733222d754642cffd67744a2b296b87c5e30207a
Vishal Dolare <mobile.vishal17@gmail.com>
2024-03-27 19:09:04 +0530

    First commit

--- src/main/java/com/java/fileoperation/GitOperations.java
+++ src/main/java/com/java/fileoperation/GitOperations.java
@@ -2,6 +2,9 @@ package com.java.fileoperation;
 
 public class GitOperations {
 
+	public static void m1() {
+		System.out.println("First commit...!");
+	}
 	public static void main(String[] args) {
 		System.out.println("Base code...!");
 		
