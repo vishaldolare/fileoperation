6a8f1b6b0e55a7a9c4deec0840619ddc3b50a65c
Vishal Dolare <mobile.vishal17@gmail.com>
2024-03-27 19:10:51 +0530

    Second commit

--- src/main/java/com/java/fileoperation/GitOperations.java
+++ src/main/java/com/java/fileoperation/GitOperations.java
@@ -5,8 +5,13 @@ public class GitOperations {
 	public static void m1() {
 		System.out.println("First commit...!");
 	}
+	public static void m2() {
+		System.out.println("Second commit...!");
+	}
 	public static void main(String[] args) {
+		System.out.println("Before...!");
 		System.out.println("Base code...!");
+		System.out.println("After ...!");
 		
 	}
 }
